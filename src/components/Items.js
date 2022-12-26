892e1487d79872cf23efcd0b54ad683ed365bd3e
Stanley Chan <0010stanleychan@gmail.com>
2022-12-26 18:48:28 +0800

    done search function

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,11 +1,31 @@
 import Badge from "./Badge";
-import data from "../data.json";
+import data from "../data_array.json";
 
 export function Items() {
   const keys = Object.keys(data);
   return (
     <>
-      {keys.map((key) => (
+      {data.map((item) => (
+        <tr key={`tr-${item.name}`} style={{ flex: 1 }}>
+          <td>{item.name}</td>
+          <td style={{ padding: 40 }}>{item.desc}</td>
+          <td>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                alignItems: "flex-start",
+              }}
+            >
+              {item.meanings.map((meaning) => (
+                <Badge key={`badge-${meaning}`} name={meaning} />
+              ))}
+            </div>
+          </td>
+        </tr>
+      ))}
+      {/* {keys.map((key) => (
         <tr style={{ flex: 1 }}>
           <td>{key}</td>
           <td style={{ padding: 40 }}>{data[key].desc}</td>
@@ -24,7 +44,7 @@ export function Items() {
             </div>
           </td>
         </tr>
-      ))}
+      ))} */}
     </>
   );
 }
